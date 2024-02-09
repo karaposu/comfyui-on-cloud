41fa7d2ee32958eb2705410272c2dac777938a98
Enes Esvet Kuzucu <eneskuzucu@hotmail.com>
2024-02-09 18:15:05 +0300

    not a fix

--- src/install_comfyui.sh
+++ src/install_comfyui.sh
@@ -3,3 +3,5 @@
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu118
 git clone https://github.com/comfyanonymous/ComfyUI
 pip install -r ComfyUI/requirements.txt
+
+# make it work as if it doesnt run.
