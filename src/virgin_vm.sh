cefbf23bf88c72eb7525da61d6657dde8afac446
Enes Esvet Kuzucu <eneskuzucu@hotmail.com>
2024-04-04 10:58:25 +0300

    add commented codes about weird firewall bug

--- src/virgin_vm.sh
+++ src/virgin_vm.sh
@@ -28,3 +28,12 @@ source .bashrc
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu118
 echo -e "import torch\nprint(torch.cuda.is_available())\nprint(torch.cuda.get_device_name(0))" > test_cuda.py
 python test_cuda.py
+
+
+## run these lines in the vm to make sure port 8188 is open for external access
+#sudo ufw allow 8188/tcp
+#sudo ufw reload
+#sudo firewall-cmd --zone=public --add-port=8188/tcp --permanent
+#sudo firewall-cmd --reload
+#sudo apt-get install iptables
+#sudo iptables -A INPUT -p tcp --dport 8188 -j ACCEPT
